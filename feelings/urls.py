c1a4ace6317c11c774e04facd217845081d9f7cf
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-20 21:22:15 +0100

    Adding thoughts from dashboard

--- feelings/urls.py
+++ feelings/urls.py
@@ -4,10 +4,12 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 
 from users import urls as user_urls
+from thoughts import urls as thoughts_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^users/', include(user_urls, namespace='users')),
+    url(r'^thoughts/', include(thoughts_urls, namespace='thoughts')),
     url(r'$', TemplateView.as_view(template_name='index.html'), name='home'),
 
 ]
