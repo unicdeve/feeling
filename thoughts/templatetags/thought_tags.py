f799df1834b3de6bc7afbfa452f4beb7d46843c8
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-21 17:39:54 +0100

    Graph for aggregate of thoughts

--- thoughts/templatetags/thought_tags.py
+++ thoughts/templatetags/thought_tags.py
@@ -1,4 +1,7 @@
 from django import template
+from django.utils import timezone
+import datetime
+import json
 
 from ..forms import ThoughtForm
 
@@ -8,3 +11,17 @@ register = template.Library()
 def thought_form():
   form = ThoughtForm
   return {'form': form}
+
+@register.simple_tag(takes_context=True)
+def chart_data(context):
+  user = context['user']
+  ten_days_ago = timezone.now() - datetime.timedelta(days=10)
+  thoughts =  user.thoughts.filter(recorder_at__gte=ten_days_ago)
+  return json.dumps({
+    'labels': [
+      thought.recorder_at.strftime('%Y-%m-%d') for thought in thoughts
+    ],
+    'series': [[
+      thought.conditions for thought in thoughts
+    ]]
+  })
