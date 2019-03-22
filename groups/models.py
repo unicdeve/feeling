6a996504ccac4373146853d2ca2eb55aa84b8b88
Taiwo Ogunola <uniqueomokenny@gmail.com>
2019-03-22 21:17:50 +0100

    Invites models to Family and Company

--- groups/models.py
+++ groups/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+import uuid
+
 from autoslug import AutoSlugField
 
 class Group(models.Model):
@@ -28,3 +30,24 @@ class Company(Group):
 
   def __str__(self):
     return self.name
+
+class Invite(models.Model):
+  from_user = models.ForeignKey(User, related_name='%(class)s_created')
+  to_user = models.ForeignKey(User, related_name='%(class)s_received')
+  accepted = models.BooleanField(default=False)
+  uuid = models.CharField(max_length=32, default='')
+
+  class Meta:
+    abstract = True
+
+  def save(self, *args, **kwargs):
+    if not self.pk:
+      self.uuid = uuid.uuid4.hex
+    super().save(*args, **kwargs)
+
+
+class CompanyInvite(Invite):
+  company = models.ForeignKey(Company, related_name='invites')
+
+class FamilyInvite(Invite):
+  family = models.ForeignKey(Family, related_name='invites')
